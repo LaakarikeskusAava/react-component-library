ea0c0bcc0ba33f094ff3fd516f29614dc23cbb26
Juho G <juho.gebhard@druid.fi>
2017-05-16 16:24:43 +0700

    IAV-2299: Profile (#12)
    * Input border color
    
    * Allow giving Section title
    
    * Input variation: width 100%
    
    * Fix coding standard violations
    
    * Add full width configuration
    
    * Version 0.1.9

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,12 +3,28 @@ import PropTypes from 'prop-types';
 import './Section.scss';
 
 export const Section = (props) => {
+  const { sectionTitle, sectionTitleLevel } = props;
+
+  let sectionTitleMarkup = null;
+  if (sectionTitle) {
+    if (sectionTitleLevel === 1) {
+      sectionTitleMarkup = (<h1 className="section__title">{sectionTitle}</h1>);
+    } else {
+      sectionTitleMarkup = (<h2 className="section__title">{sectionTitle}</h2>);
+    }
+  }
+
   return (
-    <div className="section">{props.children}</div>
+    <div className="section">
+      { sectionTitleMarkup }
+      { props.children }
+    </div>
   );
 };
 
 Section.propTypes = {
+  sectionTitle: PropTypes.string,
+  sectionTitleLevel: PropTypes.number,
   children: PropTypes.node
 };
 
