e23e932ae92e3d664dae78cc04b060a31fb4086f
Lauri Eskola <lauri.eskola@gmail.com>
2017-06-28 17:21:06 +0700

    IAV-X: Make Input component with state to allow using refs (#18)

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,20 +3,25 @@ import Classnames from 'classnames';
 import PropTypes from 'prop-types';
 import './Input.scss';
 
-const Input = ({ className, modifierClass, fullWidth, error, type, ...childProps }) => {
-  const classes = Classnames(
-    'input',
-    `input--${type}`,
-    className,
-    modifierClass,
-    fullWidth ? 'input--full-width' : null,
-    error ? 'has-error' : null
-  );
+// This component must have state because some input elements might have to use refs.
+// eslint-disable-next-line react/prefer-stateless-function
+class Input extends React.Component {
+  render() {
+    const { className, modifierClass, fullWidth, error, type, ...childProps } = this.props;
+    const classes = Classnames(
+      'input',
+      `input--${type}`,
+      className,
+      modifierClass,
+      fullWidth ? 'input--full-width' : null,
+      error ? 'has-error' : null
+    );
 
-  return (
-    <input className={classes} type={type} {...childProps} />
-  );
-};
+    return (
+      <input className={classes} type={type} {...childProps} />
+    );
+  }
+}
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
